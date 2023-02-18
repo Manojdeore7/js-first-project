e1856c8915a459a36a7927d00f6deed3555404c6
Manoj Deore <112100835+Manojdeore7@users.noreply.github.com>
2023-02-18 19:41:32 +0530

    first commit changes

---
+++ demo.js
@@ -0,0 +1,57 @@
+let nme=document.getElementById("name");
+let email=document.getElementById("email");
+let cat=document.getElementById("category");
+let sub=document.getElementById("submit");
+var arr=JSON.parse(localStorage.getItem("data"))||[];
+sub.addEventListener("click",(ex)=>{
+    ex.preventDefault();
+    let obj={
+        name:nme.value,
+        email:email.value,
+        category:cat.value
+    };
+    
+    arr.push(obj);
+    localStorage.setItem("data",JSON.stringify(arr));
+    let tbody=document.getElementById("tbody");
+    tbody.innerHTML="";
+    arr.forEach((e)=>{
+    let row=document.createElement("tr");
+    let col1=document.createElement("td");
+    col1.innerText=e.name;
+    let col2=document.createElement("td");
+    col2.innerText=e.email;
+    let colc=document.createElement("td");
+    colc.innerText=e.category;
+    let col3=document.createElement("td");
+    col3.innerText="delete";
+    //col3.className="btn btn-danger";
+    
+    col3.addEventListener("click",(el)=>{
+        el.target.parentNode.parentNode.removeChild(el.target.parentNode);
+       let ar=JSON.parse(localStorage.getItem("data"))||[];
+       arr=ar;
+        let a=ar.filter((ele)=>{
+            return e.name!=ele.name;
+        })
+        localStorage.setItem("data",JSON.stringify(a));
+    })
+   let col4=document.createElement("td");
+   col4.innerText="edit";
+   col4.addEventListener("click",(el)=>{
+    el.target.parentNode.parentNode.removeChild(el.target.parentNode);
+    let ar=JSON.parse(localStorage.getItem("data"))||[];
+    arr=ar;
+    nme.value=e.name;
+    email.value=e.email;
+     let a=ar.filter((ele)=>{
+         return e.name!=ele.name;
+     })
+     localStorage.setItem("data",JSON.stringify(a));
+})
+    row.append(col1,col2,colc,col3,col4);
+    tbody.append(row);
+})
+})
+
+//deplay data on ui
